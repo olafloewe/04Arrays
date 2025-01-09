ebadd3f902fd003d5551492922a96d200c3e7d8c
Olaf Loewe <olafloewe95@gmail.com>
2025-01-09 17:18:50 +0100

    Task 2 complete

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_2 {
     internal class Program {
+
         static void Main(string[] args) {
+            int length = 0;
+            do {
+                Console.Write("How many numbers do you want to enter?");
+            }while(!int.TryParse(Console.ReadLine(), out length));
+            int[] numbers = new int[length];
+            InputArray(numbers);
+            ReversePrintArray(numbers);
+        }
+
+        // prints the array and the average of the numbers
+        private static void ReversePrintArray(int[] nums) {
+            float total = 0.0f;
+            Console.Write("The average of numbers ");
+            
+            // loop through array printing every index and adding it to the total
+            for (int i = 0; i < nums.Length; i++) {
+                total += nums[i];
+                Console.Write($"{nums[i]} ");
+            }
+
+            Console.WriteLine($"is {total / nums.Length}");
+        }
+
+        // asks the user to fill an array of fixed length with integers
+        private static int[] InputArray(int[] nums) {
+            for (int i = 0; i < nums.Length; i++) {
+                Console.Write($"Please input number {i + 1}: ");
+                if (!int.TryParse(Console.ReadLine(), out nums[i])) i--;
+            }
+            return nums;
         }
     }
 }
