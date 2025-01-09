bdae2419d7b2e1dc84f562310cfefc565f6f7af8
Olaf Loewe <olafloewe95@gmail.com>
2025-01-09 16:59:49 +0100

    Task 1 completed

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_1 {
     internal class Program {
+
         static void Main(string[] args) {
+            int[] numbers = new int[20];
+            InputArray(numbers);
+            ReversePrintArray(numbers);
+        }
+
+        // prints the array in reverse
+        private static void ReversePrintArray(int[] nums) {
+            for (int i = nums.Length-1; i >= 0; i--) {
+                Console.WriteLine(nums[i]);
+            }
+        }
+
+        // asks the user to fill an array of fixed length with integers
+        private static int[] InputArray(int[] nums) {
+            for (int i = 0; i < nums.Length; i++) {
+                Console.WriteLine($"Please input number {i+1}:");
+                if(!int.TryParse(Console.ReadLine(), out nums[i])) i--;
+            }
+            return nums;
         }
     }
 }
