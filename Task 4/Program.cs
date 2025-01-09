bf7e8f230e1c0c57552314bd4a1292498e9e2b02
Olaf Loewe <olafloewe95@gmail.com>
2025-01-09 19:31:07 +0100

    task 4 complete

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_4 {
     internal class Program {
+
         static void Main(string[] args) {
+            int length = 0;
+            do {
+                Console.Write("How many numbers do you want to enter?");
+            } while (!int.TryParse(Console.ReadLine(), out length));
+            int[] numbers = new int[length];
+            
+            // input
+            InputArray(numbers);
+            Console.Write("Original Array: ");
+            PrintArray(numbers);
+            
+            // computing
+            BubbleSort(numbers);
+
+            // output
+            Console.Write("\nSorted Array: ");
+            PrintArray(numbers);
+        }
+
+        // prints the passed array
+        private static void PrintArray(int[] nums) {
+            for (int i = 0; i < nums.Length; i++) {
+                Console.Write($"{nums[i]} ");
+            }
+        }
+
+        // asks the user to fill an array with integers
+        private static void InputArray(int[] nums) {
+            for (int i = 0; i < nums.Length; i++) {
+                Console.Write($"Please input number {i + 1}: ");
+                if (!int.TryParse(Console.ReadLine(), out nums[i])) i--;
+            }
+        }
+
+        // sorts the passed array 
+        private static void BubbleSort(int[] nums) {
+            int largest = nums[0];
+            int tmp;
+            // loop through every number
+            for (int i = nums.Length - 1; i > 0; i--) {
+                for (int j = 1; j < nums.Length; j++) {
+                    if (nums[j] <= nums[j - 1]) Swap(nums, j, j - 1);
+                }
+            }
+        }
+
+        // swaps values of given indexes in an array
+        private static void Swap(int[] nums, int first, int second) {
+            int tmp = nums[second];
+            nums[second] = nums[first];
+            nums[first] = tmp;
         }
     }
 }
